fb241dea56210828ab694f2127d80a146d3ddb0f
Ashish Madan <madaansahaab777@gmail.com>
2022-10-08 23:48:45 +0530

    Online Chat App

---
+++ client/build/precache-manifest.a4cf4e7f29ff73db1838a9771cb82b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cefc88889aa669aaca69709b8a3bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea4ddcfbceed315de64e",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "207e3e86009cdc43cc9e",
+    "url": "/static/js/2.6e6ae44f.chunk.js"
+  },
+  {
+    "revision": "ea4ddcfbceed315de64e",
+    "url": "/static/js/main.b98269d0.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
